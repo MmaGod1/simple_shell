1a92e76ebd26624e0d249bfb863ec561f3a14f2c
root <gmmaduchukwu@gmail.com>
2025-10-08 10:32:17 -0700

    Use malloc, realloc not allowed

--- getline.c
+++ getline.c
@@ -9,37 +9,51 @@
  */
 char *_getline(void)
 {
-	static char buffer[READ_BUF_SIZE];
-	static ssize_t buf_pos, buf_size;
-	char *line = NULL;
-	size_t line_len = 0;
-	char c;
+    static char buffer[READ_BUF_SIZE];
+    static ssize_t buf_pos, buf_size;
+    char *line = NULL, *new_line;
+    size_t line_len = 0;
+    char c;
 
-	while (1)
-	{
-		/* Refill buffer if empty */
-		if (buf_pos >= buf_size)
-		{
-			buf_size = read(STDIN_FILENO, buffer, READ_BUF_SIZE);
-			buf_pos = 0;
-			if (buf_size <= 0) /* EOF or error */
-				return (line);
-		}
+    while (1)
+    {
+        /* Refill buffer if empty */
+        if (buf_pos >= buf_size)
+        {
+            buf_size = read(STDIN_FILENO, buffer, READ_BUF_SIZE);
+            buf_pos = 0;
+            if (buf_size <= 0)
+            {
+                free(line);
+                return (NULL);
+            }
+        }
 
-		/* Get current character */
-		c = buffer[buf_pos++];
+        /* Get current character */
+        c = buffer[buf_pos++];
 
-		/* Append character to the line */
-		line = realloc(line, line_len + 2);
-		if (!line)
-			return (NULL);
-		line[line_len++] = c;
-		line[line_len] = '\0';
+        /* Expand line buffer manually */
+        new_line = malloc(line_len + 2);
+        if (!new_line)
+        {
+            free(line);
+            return (NULL);
+        }
 
-		/* Stop at newline */
-		if (c == '\n')
-			break;
-	}
+        if (line)
+        {
+            memcpy(new_line, line, line_len);
+            free(line);
+        }
 
-	return (line);
+        line = new_line;
+        line[line_len++] = c;
+        line[line_len] = '\0';
+
+        /* Stop at newline */
+        if (c == '\n')
+            break;
+    }
+
+    return (line);
 }
