c1c723c204616a04bd451e4bc701ba8d00bfceda
root <gmmaduchukwu@gmail.com>
2025-10-08 10:41:21 -0700

    memcpy() not allowed

--- getline.c
+++ getline.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
 
 #define READ_BUF_SIZE 1024
 
@@ -9,51 +11,53 @@
  */
 char *_getline(void)
 {
-    static char buffer[READ_BUF_SIZE];
-    static ssize_t buf_pos, buf_size;
-    char *line = NULL, *new_line;
-    size_t line_len = 0;
-    char c;
+	static char buffer[READ_BUF_SIZE];
+	static ssize_t buf_pos, buf_size;
+	char *line = NULL, *new_line;
+	size_t line_len = 0;
+	char c;
+	ssize_t j;
 
-    while (1)
-    {
-        /* Refill buffer if empty */
-        if (buf_pos >= buf_size)
-        {
-            buf_size = read(STDIN_FILENO, buffer, READ_BUF_SIZE);
-            buf_pos = 0;
-            if (buf_size <= 0)
-            {
-                free(line);
-                return (NULL);
-            }
-        }
+	while (1)
+	{
+		/* refill buffer if empty */
+		if (buf_pos >= buf_size)
+		{
+			buf_size = read(STDIN_FILENO, buffer, READ_BUF_SIZE);
+			buf_pos = 0;
+			if (buf_size <= 0)
+			{
+				free(line);
+				return (NULL);
+			}
+		}
 
-        /* Get current character */
-        c = buffer[buf_pos++];
+		/* read one char */
+		c = buffer[buf_pos++];
 
-        /* Expand line buffer manually */
-        new_line = malloc(line_len + 2);
-        if (!new_line)
-        {
-            free(line);
-            return (NULL);
-        }
+		/* allocate new space manually */
+		new_line = malloc(line_len + 2);
+		if (!new_line)
+		{
+			free(line);
+			return (NULL);
+		}
 
-        if (line)
-        {
-            memcpy(new_line, line, line_len);
-            free(line);
-        }
+		/* manually copy old content */
+		for (j = 0; j < (ssize_t)line_len; j++)
+			new_line[j] = line[j];
 
-        line = new_line;
-        line[line_len++] = c;
-        line[line_len] = '\0';
+		if (line)
+			free(line);
 
-        /* Stop at newline */
-        if (c == '\n')
-            break;
-    }
+		line = new_line;
+		line[line_len++] = c;
+		line[line_len] = '\0';
 
-    return (line);
+		/* stop on newline */
+		if (c == '\n')
+			break;
+	}
+
+	return (line);
 }
