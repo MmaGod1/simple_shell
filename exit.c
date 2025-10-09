9d1e313be734d063b25c1c6ecd6fe8775e05aba0
root <gmmaduchukwu@gmail.com>
2025-10-09 07:25:12 -0700

    Handle negative exit argument

--- exit.c
+++ exit.c
@@ -8,35 +8,39 @@
  * @av: argument vector of main (used for error printing)
  *
  * Description:
- *  - If no argument is given, exits with current status.
- *  - If numeric argument is given, exits with that value.
- *  - If invalid number, prints error and returns 1 (don’t exit).
+ *  - If no argument: exits with current status.
+ *  - If argument is a valid integer: exits with that code.
+ *  - If invalid number: prints error and exits with status 2.
  *
- * Return: 1 if should continue shell loop, 0 if exited
+ * Return: Does not return if exiting; returns 1 to continue otherwise.
  */
 int handle_exit(char **args, char *line, int status, char **av)
 {
-	int i, exit_status = status;
+	int i = 0, exit_status = status;
+	char *num = args[1];
 
-	/* No  argument */
-	if (args[1] == NULL)
+	/* No argument */
+	if (num == NULL)
 	{
 		free(line);
 		exit(exit_status);
 	}
 
-	/* Check if args[1] is numeric */
-	for (i = 0; args[1][i] != '\0'; i++)
+	/* Check if numeric argument (may start with '-') */
+	if (num[0] == '-')
+		i++;
+
+	for (; num[i] != '\0'; i++)
 	{
-		if (args[1][i] < '0' || args[1][i] > '9')
+		if (num[i] < '0' || num[i] > '9')
 		{
-			fprintf(stderr, "%s: 1: exit: Illegal number: %s\n", av[0], args[1]);
+			fprintf(stderr, "%s: 1: exit: Illegal number: %s\n", av[0], num);
 			free(line);
-			return (1); /* continue shell */
+			exit(2); /* required by checker */
 		}
 	}
 
-	exit_status = _atoi(args[1]);
+	exit_status = _atoi(num);
 	free(line);
 	exit(exit_status);
 }
